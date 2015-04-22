094c2e232eb897bf4a4b7cbff8536792c5601007
kryy3n <tudor.karin@yahoo.com>
2015-04-22 19:26:09 +0000

    commit #2

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-var app = angular.module('TestApplication', ['youtube-embed']);
+var app = angular.module('TestApplication', ['ui.router','youtube-embed']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+  $stateProvider
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
 
 app.factory('posts', [function(){
   var o = {
@@ -22,7 +43,11 @@ function($scope,posts){
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0});
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]});
     $scope.title = '';
     $scope.link = '';
   };
@@ -31,4 +56,11 @@ function($scope,posts){
   };
 }]);
 
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+}]);
 
